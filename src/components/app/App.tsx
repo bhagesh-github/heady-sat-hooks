c91484b3d374470430aafeb27297e32ebc8ae049
bhagesh.kalekar <bhagesh.kalekar@gmail.com>
2020-04-17 21:38:36 +0530

    added related products

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import Header from '../header';
 import Footer from '../footer';
+import Product from '../product';
 
 function App() {
   return (
     <div className="container">
       <Header/>
+      <Product/>
       <Footer/>
     </div>
   );
